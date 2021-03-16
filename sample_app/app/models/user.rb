84ab876c11efac144274515a55325679f6c53ef7
walczyk123 <walczyk.kamil@gmail.com>
2021-03-16 15:12:43 +0100

    uniqueness validation, made validation non case sensitive, performed tests, all is ok, for now..

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates(:name, presence: true, length: {maximum:50})
   #VER VALID_EMAIL_REGEX
   VER=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates(:email, presence: true, length: {maximum:255, format: {with: /VER/}})
+  #validation of email, it has to be input, with max length 255, fulfill regex format, be unique and no case sEnSiTiVe
+  validates(:email, presence: true, length: {maximum:255, format: {with: /VER/}}, uniqueness: {  case_sensitive: false })
 end
