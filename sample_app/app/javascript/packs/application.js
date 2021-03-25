4043e2764d7274ad84080d48f62cd018c383372f
walczyk123 <walczyk.kamil@gmail.com>
2021-03-25 19:52:50 +0100

    Completed changing layout links

--- sample_app/app/javascript/packs/application.js
+++ sample_app/app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// jquery and bootstrap for menu
+require("jquery")
+import "bootstrap"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- sample_app/config/webpack/environment.js
+++ sample_app/config/webpack/environment.js
@@ -1,3 +1,8 @@
-const { environment } = require('@rails/webpacker')
-
+const {environment} = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.prepend('Provide', new webpack.ProwidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
 module.exports = environment
