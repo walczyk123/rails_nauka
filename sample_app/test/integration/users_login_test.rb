a57aeb8845116c28b1bae9c7a8c2ac25372650a9
walczyk123 <walczyk.kamil@gmail.com>
2021-03-30 11:42:56 +0200

    Fixed Fixed a bug that caused an error when logging out of multiple browser tabs

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -53,6 +53,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user),count: 0 #no logout path and logged user after logout
   end
 
+  test "login with valid information then logout in one of few tabs" do
+    get login_path
+    post login_path, params: {session: { email: @user.email, password: "password"}}
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count:0 #login path disappears after login and redirect?
+    assert_select "a[href=?]", logout_path # logout and user path appears after login?
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    #user logout in one tab
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user),count: 0 #no logout path and logged user after logout
+  end
+
 end
 
 # rails test test/integration/users_login_test.rb
