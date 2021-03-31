bb95847e737d7455f2f8621ee897d448e4a42d89
walczyk123 <walczyk.kamil@gmail.com>
2021-03-31 20:07:08 +0200

    Added test helper methods that log in as user with params, added tests to login with and without remembering

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -74,7 +74,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user),count: 0 #no logout path and logged user after logout
   end
 
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_not_empty cookies[:remember_token]
+  end
 
+  test "login without remembering" do
+    #log and set cookie
+    log_in_as(@user, remember_me: "1")
+    # log agin, cookie is deleted?
+    log_in_as(@user, remember_me: "0")
+    assert_empty cookies[:remember_token]
+  end
 
 end
 
